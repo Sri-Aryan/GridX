d91241497bbba967bbedb9224d52bd8f3e983b37
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2025-10-16 16:32:06 +0530

    fix: Fixed Import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'pages/home_page.dart';
+import 'package:gridx/screen/home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
